0feb3d90c83a7bb8d58c48ec26c84ded7db40163
hassan <hassanlouazri@outlook.com>
2024-09-05 00:47:04 +0200

    blog list start

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class BlogListComponent {
 
+  blogs = [
+    { title: 'Blog Post 1', description: 'This is a brief description of blog post 1.', file: 'post1.md', image: '../../../assets/article.jpg' },
+    { title: 'Blog Post 2', description: 'This is a brief description of blog post 2.', file: 'post2.md', image: '../../../assets/article.jpg' },
+    { title: 'Blog Post 3', description: 'This is a brief description of blog post 3.', file: 'post3.md', image: '../../../assets/article.jpg' },
+    { title: 'Blog Post 4', description: 'This is a brief description of blog post 4.', file: 'post4.md', image: '../../../assets/free-time.jpeg' },
+    { title: 'Blog Post 5', description: 'This is a brief description of blog post 5.', file: 'post5.md', image: '../../../assets/article.jpg' },
+    { title: 'Blog Post 6', description: 'This is a brief description of blog post 6.', file: 'post6.md', image: '../../../assets/free-time.jpeg' }
+  ];
+
+  constructor(private router: Router) {}
+
+  onCardClick() {
+    console.log("button clicked");
+  }
 }
