7d4a8a4f50ed737dab530566ba3c47748afdeb43
Omar - Warrior <omar.lwazri@gmail.com>
2024-09-09 19:23:17 +0100

    Adding Animation to OfferComponent + fix hero font-size in pc/mobile

--- src/app/landing/offer/offer.component.ts
+++ src/app/landing/offer/offer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss']
 })
-export class OfferComponent {
+export class OfferComponent implements AfterViewInit{
+  isVisible = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const elements = document.querySelectorAll('.head-text') as NodeListOf<HTMLElement>;
+    elements.forEach((element) => {
+      const rect = element.getBoundingClientRect();
+
+      // Check if each text element is in the viewport (with some offset)
+      if (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        element.classList.add('visible');
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.onWindowScroll(); // Call once on initialization
+  }
 }
