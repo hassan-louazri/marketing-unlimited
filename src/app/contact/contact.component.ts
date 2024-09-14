594a2d3663d3f723c03ab564c9c5009c4ed14856
hassan <hassanlouazri@outlook.com>
2024-09-15 00:42:56 +0200

    feat: update form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  onSubmit(form: any) {
+
+  // options = ["0 - 500", "500 - 2000", "2000 - 10000", "+10000"];
+
+  // selectedAdSpend: string = "0 - 500";
+
+  constructor(private modalService: NgbModal, config: NgbModalConfig) {
+    config.animation = true;
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  // TODO: replace "adspend" field with dropdown menu with options above.
+  // selectAdSpend(option: string) {
+  //   this.selectedAdSpend = option;
+  // }
+  
+  onSubmit(form: NgForm, content: any) {
     if(form.valid) {
-      console.log("submitted");
-      console.log(form.value);
+      // TODO: write form to excel sheet
+
+      this.modalService.open(content);
+      form.resetForm();
     }
   }
 }
